7ca945bda903081409a2c6906cba1d7e120067d8
Havyx <everluca@outlook.com>
2021-03-19 16:52:11 -0300

    :sparkles: building permission  alghorithm

--- src/main/java/dev/evertonsavio/app/CryptographyApplication.java
+++ src/main/java/dev/evertonsavio/app/CryptographyApplication.java
@@ -95,7 +95,95 @@ public class CryptographyApplication {
         Long year = (newDate / 1000 / 60 / 60 / 24 / 365) + 1970;
         System.out.println(year);
         byte[] meuByte = longToBytes(year);
-        System.out.println(meuByte[7]);
+        System.out.println(Long.toHexString(year));
+        String longHEX = Long.toHexString(65535L);
+        System.out.println(longHEX);
+
+        int a = 0;
+        byte[] testByte = new byte[2];
+        for(int i = 0; i < 4; i = i+2){
+            System.out.println(Integer.parseInt(longHEX.substring(i, i+2), 16) + " " + longHEX.substring(i, i+2));
+            testByte[a] = (byte) Integer.parseInt(longHEX.substring(i, i+2), 16);
+            a = a +1;
+        }
+
+    }
+
+    private static String calculateCmac (String uuid, byte role, String uctTime, String uctDelta, String timeInit,
+                                  String timeDelta, byte days, String secret){
+
+        int a = 0;
+        byte[] secretByte = new byte[16];
+        for(int i = 0; i < 32; i = i+2){
+            //System.out.println(Integer.parseInt(uuid.substring(i, i+2), 16) + " " + uuid.substring(i, i+2));
+            secretByte[a] = (byte) Integer.parseInt(secret.substring(i, i+2), 16);
+            a = a +1;
+        }
+
+        a = 0;
+        byte[] msgByte = new byte[30];
+        for(int i = 0; i < 32; i = i+2){
+            //System.out.println(Integer.parseInt(uuid.substring(i, i+2), 16) + " " + uuid.substring(i, i+2));
+            msgByte[a] = (byte) Integer.parseInt(uuid.substring(i, i+2), 16);
+            a = a + 1;
+        }
+
+        msgByte[16] = role;
+
+		String longHEX = Long.toHexString(65535L);
+		System.out.println(longHEX);
+
+		for(int i = 0; i < 4; i = i+2){
+			//System.out.println(Integer.parseInt(uctTime.substring(i, i+2), 16) + " " + uctTime.substring(i, i+2));
+			msgByte[a] = (byte) Integer.parseInt(uctTime.substring(i, i+2), 16);
+			a = a +1;
+		}
+        for(int i = 0; i < 4; i = i+2){
+            //System.out.println(Integer.parseInt(uctDelta.substring(i, i+2), 16) + " " + uctDelta.substring(i, i+2));
+            msgByte[a] = (byte) Integer.parseInt(uctDelta.substring(i, i+2), 16);
+            a = a +1;
+        }
+        System.out.println("TIME SIZE:" + timeInit.length());
+        for(int i = 0; i < 1; i = i+1){
+            System.out.println(Integer.parseInt(timeInit.substring(i, i+1), 16) + " " + timeInit.substring(i, i+1));
+            msgByte[a] = (byte) Integer.parseInt(timeInit.substring(i, i+timeInit.length()), 16);
+            a = a + 1;
+        }
+        System.out.println("DELTA SIZE: " + timeDelta.length());
+
+
+        CipherParameters params = new KeyParameter(secretByte);
+        BlockCipher aes = new AESEngine();
+        CMac mac = new CMac(aes);
+        mac.init(params);
+        mac.update(msgByte, 0, msgByte.length);
+
+        byte[] out = new byte[mac.getMacSize()];
+        mac.doFinal(out, 0);
+
+        StringBuilder s19 = new StringBuilder();
+        for (byte b : msgByte){
+            s19.append(String.format("%02X", b));
+        }
+        System.out.println("ecrypted msg cmac: " + s19.toString());
+        for (byte b : out) {
+            s19.append(String.format("%02X", b));
+        }
+        System.out.println("ecrypted secret appended:" + s19.toString());
+
+        int d = 0;
+        byte[] testByte = new byte[s19.length()/2];
+        System.out.println(s19.length());
+        for(int i = 0; i < s19.length(); i = i+2){
+            System.out.println(Integer.parseInt(s19.substring(i, i+2), 16) + " " + s19.substring(i, i+2));
+            testByte[d] = (byte) Integer.parseInt(s19.substring(i, i+2), 16);
+            d = d +1;
+        }
+
+        System.out.println("TEST BYTE SIZE: " + testByte.length);
+        String encodedTESTBYTE = Base64.getEncoder().encodeToString(testByte);
+
+        return encodedTESTBYTE;
     }
 
     public static void main(String[] args) {
@@ -104,7 +192,13 @@ public class CryptographyApplication {
         //hex.hexStringToByte();
 
         //getCMAC();
-        getDate();
+        //getDate();
+
+        String cmac = calculateCmac("295e40e0a1ecf6f9c9127eed1358ad8c", (byte) 0x00, Long.toHexString(16975845L),
+                Long.toHexString(16975845L), Long.toHexString(0), Long.toHexString(5947L), Byte.valueOf("127"),
+                "295e40e0a1ecf6f9c1227eed1358ad8c");
+
+        System.out.println(cmac);
     }
 
 }
