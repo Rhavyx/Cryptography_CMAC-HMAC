c8790482c54424a71b940b288f3213176b5a501a
Havyx <everluca@outlook.com>
2021-03-30 11:57:17 -0300

    :rocket: aes cbc encrypt decrypt

--- src/main/java/dev/evertonsavio/app/AESEncrypyDecrypt.java
+++ src/main/java/dev/evertonsavio/app/AESEncrypyDecrypt.java
@@ -1,4 +1,4 @@
-package dev.evertonsavio.app.utils;
+package dev.evertonsavio.app;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -11,7 +11,7 @@ public class AESEncrypyDecrypt {
     private static final String initVector = "encryptionIntVec";
 
     public static byte[] getKey(String inputString){
-        byte[] byteKey = new byte[16];
+        byte[] byteKey = new byte[inputString.length()/2];
         int a = 0;
         for(int i = 0; i < inputString.length() ; i= i + 2){
             System.out.println(Integer.parseInt(inputString.substring(i, i+2), 16) + " " + inputString.substring(i, i+2));
@@ -23,7 +23,8 @@ public class AESEncrypyDecrypt {
 
     public static String encrypt(String value) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            //IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            IvParameterSpec iv = new IvParameterSpec(getKey("9280B7D1160C05F5E9A3C53A22099052"));
             //SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
             SecretKeySpec skeySpec = new SecretKeySpec(getKey("9280B7D1160C05F5E9A3C53A22099052"), "AES");
 
@@ -40,7 +41,8 @@ public class AESEncrypyDecrypt {
 
     public static String decrypt(String encrypted) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            //IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            IvParameterSpec iv = new IvParameterSpec(getKey("9280B7D1160C05F5E9A3C53A22099052"));
             //SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
             SecretKeySpec skeySpec = new SecretKeySpec(getKey("9280B7D1160C05F5E9A3C53A22099052"), "AES");
 
@@ -56,14 +58,52 @@ public class AESEncrypyDecrypt {
         return null;
     }
 
+    public static String decrypt(byte[] encrypted) {
+        try {
+            //IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            IvParameterSpec iv = new IvParameterSpec(getKey("9280B7D1160C05F5E9A3C53A22099052"));
+            //SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            SecretKeySpec skeySpec = new SecretKeySpec(getKey("9280B7D1160C05F5E9A3C53A22099052"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(encrypted);
+
+            StringBuilder s19 = new StringBuilder();
+            for(byte b: original){
+                s19.append(String.format("%02X", b));
+            }
+
+            //return new String(original);
+            return s19.toString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
 
     public static void main(String[] args) {
-        String originalString = "password";
-        System.out.println("Original String to encrypt - " + originalString);
-        String encryptedString = encrypt(originalString);
+//        String originalString = "password";
+//        System.out.println("Original String to encrypt - " + originalString);
+//        String encryptedString = encrypt(originalString);
+
+        byte[] encryptedString = getMsgBytes("5DC97095BB7EBE81A7D33E7827D5C6080113486AD70A1217709D050006201EE2");
+
         System.out.println("Encrypted String - " + encryptedString);
         String decryptedString = decrypt(encryptedString);
         System.out.println("After decryption - " + decryptedString);
+    }
+
+    public static byte[] getMsgBytes(String inputString){
+        byte[] byteKey = new byte[inputString.length()/2];
+        int a = 0;
+        for(int i = 0; i < inputString.length() ; i= i + 2){
+            System.out.println(Integer.parseInt(inputString.substring(i, i+2), 16) + " " + inputString.substring(i, i+2));
+            byteKey[a] = (byte) Integer.parseInt(inputString.substring(i, i+2), 16);
+            a++;
+        }
+        return byteKey;
     }
 
 }
